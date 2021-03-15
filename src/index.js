94fb421314412a980b2124fca38c17d9e030ccdc
Vitali Herasimenka <vitttold@inbox.ru>
2021-03-15 18:54:23 +0300

    add: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let newArr = bracketsConfig.map((item) => item.join(""));
+    let newArr2 = [];
+    for (let i = 0; i < newArr.length; i++) {
+        if (str.includes(newArr[i])) {
+           // newArr2 = newStr.push(newArr[i]);
+             str = str.replace(newArr[i], "");
+             i = -1;
+        }
+    }
+
+    if (str.length  === 0) {
+        return true;
+    }
+    return false;
+};
